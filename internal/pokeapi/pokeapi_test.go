985da0418a02acaa461c3c279b0ea96785663ba2
Caleb Fringer <cfringer98@gmail.com>
2025-01-21 22:31:29 -0800

    Fixed TestGetLocationArea that was broken by refactoring.

--- internal/pokeapi/pokeapi_test.go
+++ internal/pokeapi/pokeapi_test.go
@@ -3,23 +3,15 @@ package pokeapi
 import "testing"
 
 func TestGetLocationArea(t *testing.T) {
-	endpoint := PageLink{
-		"https://pokeapi.co/api/v2/location-area",
-		"",
-	}
+	endpoint := "https://pokeapi.co/api/v2/location-area"
 
-	results, err := GetLocationArea(&endpoint)
+	response, err := GetLocationArea(endpoint)
 	if err != nil {
-		t.Fatalf("Querying %s returned an error", endpoint.Next)
+		t.Fatalf("Querying %s returned an error", endpoint)
 	}
 
-	if results[0].Name != "canalave-city-area" {
-		t.Fatalf("Querying the first location-area returned wrong area.\n\tExpected: %s\n\tFound: %s", results[0].Name, "canalave-city-area")
-	}
-
-	if endpoint.Next != "https://pokeapi.co/api/v2/location-area?offset=20&limit=20" {
-		t.Fatalf("Failed to update the pageLink structure correctly.\n\tExpected: %s\n\tFound: %s",
-			"https://pokeapi.co/api/v2/location-area/?offset=20&limit=20",
-			endpoint.Next)
+	firstArea := response.Results[0].Name
+	if firstArea != "canalave-city-area" {
+		t.Fatalf("Querying the first location-area returned wrong area.\n\tExpected: %s\n\tFound: %s", firstArea, "canalave-city-area")
 	}
 }
