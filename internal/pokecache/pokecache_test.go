61d963ba2af06a02834c124f9cad6a7ff2faf1db
Caleb Fringer <cfringer98@gmail.com>
2025-01-22 21:00:28 -0800

    Fixed cache unit tests after changing key type to url.URL.

--- internal/pokecache/pokecache_test.go
+++ internal/pokecache/pokecache_test.go
@@ -1,31 +1,46 @@
 package pokecache
 
 import (
+	"fmt"
+	"net/url"
+	"os"
 	"testing"
 	"time"
 )
 
+var testUrl *url.URL
+
+func init() {
+	var err error
+	testUrl, err = url.Parse("http://test.go/path/to/endpoint")
+	if err != nil {
+		fmt.Println("Error initializing test url.")
+		os.Exit(1)
+	}
+}
+
 func TestAdd(t *testing.T) {
 	cache := NewCache(5 * time.Second)
-	cache.Add("Test", []byte("This is test data"))
+	cache.Add(*testUrl, []byte("This is test data"))
 }
 
 func TestGet(t *testing.T) {
 	cache := NewCache(5 * time.Second)
-	cache.Add("Hiya", []byte{})
-	if _, ok := cache.Get("Hiya"); !ok {
+	cache.Add(*testUrl, []byte{})
+	time.Sleep(time.Second)
+	if _, ok := cache.Get(*testUrl); !ok {
 		t.Fatal("Reap loop removed the entry before time was up.")
 	}
 }
 
 func TestReapLoop(t *testing.T) {
 	cache := NewCache(5 * time.Second)
-	cache.Add("test", []byte{})
-	if _, ok := cache.Get("test"); !ok {
+	cache.Add(*testUrl, []byte{})
+	if _, ok := cache.Get(*testUrl); !ok {
 		t.Fatal("Reap loop removed the entry before time was up.")
 	}
 	time.Sleep(5 * time.Second)
-	if _, ok := cache.Get("test"); ok {
-		t.Fatalf("Reap loop failed to remove test entry")
+	if _, ok := cache.Get(*testUrl); ok {
+		t.Fatalf("Reap loop failed to remove *test entry")
 	}
 }
